685c7d067964d27e534798f6d6e986eeaad46d8d
DEVita42 <dvt.lorenzo@gmail.com>
2024-10-20 20:22:11 +0200

    react-router-2

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './Welcome';
+import Counter from './Counter'; 
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome name="User" age={25} />} />
+        <Route path="/counter" element={<Counter />} /> 
       </Routes>
     </Router>
   );
