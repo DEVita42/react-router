68498f1a5ef74cb562d06162880d094569af8b85
DEVita42 <dvt.lorenzo@gmail.com>
2024-10-20 20:55:23 +0200

    react-router-4

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Welcome from './Welcome';
 import Counter from './Counter';
 import ShowGithubUser from './ShowGithubUser';
+
 const App = () => {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/counter">Counter</Link>
+          </li>
+          <li>
+            <Link to="/users/octocat">GitHub User</Link> 
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Welcome name="User" age={25} />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="/users/:username" element={<ShowGithubUser />} /> 
+        <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
     </Router>
   );
